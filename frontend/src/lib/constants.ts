bedbcdd7c5c7643deb48bd9d5829a05bff8a8408
LucianoRocchetta <lucianorocchetta@gmail.com>
2024-09-20 17:12:14 -0300

    Add(Frontend): Images & Lib folder added with constants & Images files

---
+++ frontend/src/lib/constants.ts
@@ -0,0 +1,17 @@
+export const benefits = [
+    {
+        key: 0,
+        title: "Trusted and Verified Resources",
+        description: "We thoroughly vet and verify every mental health resource in our network, ensuring you only have access to reputable and reliable options."
+    }, 
+    {
+        key: 1,
+        title: "Up-to-Date Information",
+        description: "Our team continuously monitors and updates our listings, so you can always count on receiving the most current and relevant information."
+    }, 
+    {
+        key: 2,
+        title: "Personalized Search Options",
+        description: "With our advanced filters, you can easily find mental health services tailored to your specific needs, whether it’s based on location, type of therapy, insurance coverage, or condition specialty."
+    }
+]
