7bff5afe86ba90b90194df85d7bc21424cf1261c
LucianoPED <luciano@poreldeporte.com>
2024-09-20 18:24:01 -0300

    Add(Frontend): Hero section added

--- frontend/src/routes/homepage/components/hero/index.tsx
+++ frontend/src/routes/homepage/components/hero/index.tsx
@@ -1,3 +1,23 @@
+import { Typography, Button } from "@/components/ui";
+
 export const Hero = () => {
-  return <div>index</div>;
+
+  
+  const handleTest = (text: string, event: React.MouseEvent): void => {
+    console.log(text, event)
+  };
+
+  return (
+  <section className="page-width mb-5">
+      <Typography className="font-antic my-4" as="h1" color="black" variant="title">
+        A network curated for your <span className="text-green-300">path to healing</span>
+      </Typography>
+
+      <Typography className="mb-2" as="p" color="darkGray" variant="medium">
+      Explore our mindfully curated network of trusted mental health resources to find the support you need, when you need it.
+      </Typography>
+
+      <Button onClick={(e) => {handleTest("HelloWorld", e)}} className="mt-4" variant="medium">Start Exploring</Button>
+  </section>
+  );
 };
