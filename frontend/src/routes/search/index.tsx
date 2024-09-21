283b6a2c466afa4b71ca9b7f713d2b66913fb7d4
marfogped <martin@poreldeporte.com>
2024-09-21 13:48:22 -0300

    add(frontend): constants & testing logic to get data from params

--- frontend/src/routes/search/index.tsx
+++ frontend/src/routes/search/index.tsx
@@ -1,6 +1,14 @@
+import { useSearchParams } from "react-router-dom";
+
 export function SearchPage() {
+  const [searchParams] = useSearchParams();
+
+  const resource = searchParams.get("resource");
+  const age = searchParams.get("age");
+
+  console.log(resource, age);
   return (
-    <section>
+    <section className="page-width">
       <div>Search Page</div>
     </section>
   );
