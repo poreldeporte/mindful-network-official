55e7e167cffbda5f260831c1b987ce278af1a882
marfogped <martin@poreldeporte.com>
2024-09-23 17:02:37 -0300

    fix(client): images tags replced for Image component from Next

--- client/src/routes/homepage/components/about/index.tsx
+++ client/src/routes/homepage/components/about/index.tsx
@@ -1,4 +1,5 @@
 import { Typography } from "@/components/ui";
+import Image from "next/image";
 import { Person1, Person2, Person3 } from "@/lib/images";
 import { benefits } from "@/lib/constants";
 
@@ -11,7 +12,12 @@ interface BenefitCardProps {
 const BenefitCard = ({ title, description }: BenefitCardProps) => {
   return (
     <article>
-      <Typography className="mb-2 font-bold" color="black" as="h4" variant="medium">
+      <Typography
+        className="mb-2 font-bold"
+        color="black"
+        as="h4"
+        variant="medium"
+      >
         {title}
       </Typography>
 
@@ -27,30 +33,37 @@ export function About() {
     <section className="page-width my-4">
       <div>
         <Typography className="mb-2" color="black" as="h2" variant="subtitle">
-          <span className="text-green-300">We' re here to help</span> <span className="block"/>build your support net
+          <span className="text-green-300">We' re here to help</span>{" "}
+          <span className="block" />
+          build your support net
         </Typography>
 
-        <Typography className="mb-2 font-semibold" color="black" as="h3" variant="medium">
+        <Typography
+          className="mb-2 font-semibold"
+          color="black"
+          as="h3"
+          variant="medium"
+        >
           Access the most reliable and reputable mental health resources
           available
         </Typography>
 
         <div className="my-5">
-        {benefits.map((benefit) => {
-          return (
-            <BenefitCard
-              key={benefit.key}
-              title={benefit.title}
-              description={benefit.description}
-            />
-          );
-        })}
+          {benefits.map((benefit) => {
+            return (
+              <BenefitCard
+                key={benefit.key}
+                title={benefit.title}
+                description={benefit.description}
+              />
+            );
+          })}
         </div>
       </div>
       <div className="grid grid-cols-3 gap-5">
-        <img className="w-full" src={Person1} alt=""></img>
-        <img className="w-full" src={Person2} alt=""></img>
-        <img className="w-full" src={Person3} alt=""></img>
+        <Image className="w-auto h-auto" src={Person1} alt="Person 1" />
+        <Image className="w-auto h-auto" src={Person2} alt="Person 2" />
+        <Image className="w-auto h-auto" src={Person3} alt="Person 3" />
       </div>
     </section>
   );
