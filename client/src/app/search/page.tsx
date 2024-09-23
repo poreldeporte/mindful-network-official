b33992e97cb929b59a8e23da2b267e4d0179eaa1
marfogped <martin@poreldeporte.com>
2024-09-23 17:16:27 -0300

    feat(client): Map component imported

--- client/src/app/search/page.tsx
+++ client/src/app/search/page.tsx
@@ -1,11 +1,24 @@
 import { Typography } from "@/components/ui";
+import { MapComponent } from "@/components/shared";
 
 export default function Search() {
   return (
     <main className="">
-      <Typography className="font-antic" as="h2" color="black" variant="medium">
-        Search Page
-      </Typography>
+      <section className="h-screen bg-orange-200 mt-60">
+        <Typography
+          className="font-antic"
+          as="h2"
+          color="black"
+          variant="medium"
+        >
+          Search component
+        </Typography>
+
+        <MapComponent
+          position={[34.0522, -118.2437]}
+          className="h-full w-full"
+        />
+      </section>
     </main>
   );
 }
