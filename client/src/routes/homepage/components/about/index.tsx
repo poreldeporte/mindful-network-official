e4bc8aafd30111a11e42b43bd178903db5b3e517
LucianoPED <luciano@poreldeporte.com>
2024-09-24 18:26:08 -0300

    Fix(Client): Adding user photos positions

--- client/src/routes/homepage/components/about/index.tsx
+++ client/src/routes/homepage/components/about/index.tsx
@@ -30,41 +30,52 @@ const BenefitCard = ({ title, description }: BenefitCardProps) => {
 
 export function About() {
   return (
-    <section className="page-width my-4">
-      <div>
-        <Typography className="mb-2" color="black" as="h2" variant="subtitle">
-          <span className="text-green-300">We' re here to help</span>{" "}
-          <span className="block" />
-          build your support net
-        </Typography>
+    <section className="page-width my-56">
+      <div className="flex flex-col lg:flex-row lg:items-center justify-between">
+        <div>
+          <Typography className="mb-2" color="black" as="h2" variant="subtitle">
+            <span className="text-green-300">We're here to help</span>{" "}
+            <span className="block" />
+            build your support net
+          </Typography>
 
-        <Typography
-          className="mb-2 font-semibold"
-          color="black"
-          as="h3"
-          variant="medium"
-        >
-          Access the most reliable and reputable mental health resources
-          available
-        </Typography>
+          <Typography className="mb-2 font-semibold" color="black" as="h3" variant="medium">
+            Access the most reliable and reputable mental health resources available
+          </Typography>
 
-        <div className="my-5">
-          {benefits.map((benefit) => {
-            return (
-              <BenefitCard
-                key={benefit.key}
-                title={benefit.title}
-                description={benefit.description}
-              />
-            );
-          })}
+          <div className="my-5">
+            {benefits.map((benefit) => {
+              return (
+                <BenefitCard
+                  key={benefit.key}
+                  title={benefit.title}
+                  description={benefit.description}
+                />
+              );
+            })}
+          </div>
+        </div>
+
+        <div className="relative w-full">
+          <div className="grid grid-cols-3 gap-5 lg:relative">
+            <Image
+              className="w-auto h-auto lg:absolute lg:top-0 lg:left-0 lg:w-1/3 lg:h-auto"
+              src={Person1}
+              alt="Person 1"
+            />
+            <Image
+              className="w-auto h-auto lg:absolute lg:top-10 lg:left-1/3 lg:w-1/3 lg:h-auto"
+              src={Person2}
+              alt="Person 2"
+            />
+            <Image
+              className="w-auto h-auto lg:absolute lg:bottom-0 lg:right-0 lg:w-1/3 lg:h-auto"
+              src={Person3}
+              alt="Person 3"
+            />
+          </div>
         </div>
       </div>
-      <div className="grid grid-cols-3 gap-5">
-        <Image className="w-auto h-auto" src={Person1} alt="Person 1" />
-        <Image className="w-auto h-auto" src={Person2} alt="Person 2" />
-        <Image className="w-auto h-auto" src={Person3} alt="Person 3" />
-      </div>
-    </section>
+</section>
   );
 }
