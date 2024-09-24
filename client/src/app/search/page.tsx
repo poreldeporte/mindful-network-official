97bc9ebb4ad1a30e0959daa33916ea987228b9de
marfogped <martin@poreldeporte.com>
2024-09-24 17:38:04 -0300

    add(client): Search Wrapper component imported

--- client/src/app/search/page.tsx
+++ client/src/app/search/page.tsx
@@ -1,24 +1,9 @@
-import { MapComponent } from "@/components/shared";
-import { PsychologistModel } from "@/models";
-import { SidePanel } from "@/routes/search";
-import { getAllPsychologists } from "@/services/psychologist.service";
-import { getAllConditions } from "@/services/condition-specialties.service";
+import { SearchWrapper } from "@/routes/search";
 
-export default async function Search() {
-  const psychologists: PsychologistModel[] = await getAllPsychologists();
-  const conditions: any = await getAllConditions();
-
-  console.log(conditions);
-
+export default function Search() {
   return (
-    <>
-      <section className="h-screen mt-10 relative">
-        <SidePanel psychologists={psychologists} />
-        <MapComponent
-          position={[34.0522, -118.2437]}
-          className="h-full w-full"
-        />
-      </section>
-    </>
+    <section className="h-screen mt-10 relative">
+      <SearchWrapper />
+    </section>
   );
 }
