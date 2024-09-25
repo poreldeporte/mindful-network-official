6c0cb2d836ab43d050b342387c6e01de9245d53b
marfogped <martin@poreldeporte.com>
2024-09-25 00:11:24 -0300

    fix(client): Homepage text

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,15 @@
 import { GetInTouch } from "@/components/forms";
 import { Typography } from "@/components/ui";
-import { About, BlogContainer, CTASection, Hero } from "@/routes/homepage/components";
+import {
+  About,
+  BlogContainer,
+  CTASection,
+  Hero,
+} from "@/routes/homepage/components";
 
 export default function Home() {
   return (
-    <main className="">
-      <Typography className="font-antic" as="h2" color="black" variant="medium">
-        Homepage
-      </Typography>
+    <main>
       <Hero />
       <About />
       <BlogContainer />
