1c6d5a6c44520d99cbfef5258a98b44e70e5b9e3
marfogped <martin@poreldeporte.com>
2024-09-27 13:59:47 -0300

    fix(client): utilities to avoid import errors

--- client/src/app/api/psychologists/utilities/findDocumentByName.utility.ts
+++ client/src/app/api/psychologists/utilities/findDocumentByName.utility.ts
@@ -1,2 +1,17 @@
-export const findDocumentByName = (array, name) =>
-  array.find((el) => el.name.toLowerCase() === name.toLowerCase());
+export const findDocumentByName = (array, name) => {
+  console.log("=== Array", array);
+  console.log("=== name", name);
+
+  return array.find((el) => {
+    console.log("=== Element", el);
+    const elName = el.name ? el.name.toLowerCase() : "";
+    const elType = el.type ? el.type.toLowerCase() : "";
+    const elAge = el.age ? el.age.toString().toLowerCase() : "";
+
+    return (
+      elName === name.toLowerCase() ||
+      elType === name.toLowerCase() ||
+      elAge === name.toLowerCase()
+    );
+  });
+};
