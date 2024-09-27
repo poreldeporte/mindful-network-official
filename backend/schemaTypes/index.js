02601dde3082d11de6b883a53c602bb59dd7d62c
marfogped <martin@poreldeporte.com>
2024-09-27 07:53:13 -0300

    add(backend): user schema

--- backend/schemaTypes/index.js
+++ backend/schemaTypes/index.js
@@ -1,13 +1,15 @@
-import pshychologists from "./psychologists/pshychologists"
-import ageSpecialties from "./age-specialties/ageSpecialties"
-import conditionSpecialties from "./condition-specialties/conditionSpecialties"
-import insurances from "./insurances/insurances"
-import therapyModalities from "./therapy-modalities/therapyModalities"
+import pshychologists from './psychologists/pshychologists'
+import ageSpecialties from './age-specialties/ageSpecialties'
+import conditionSpecialties from './condition-specialties/conditionSpecialties'
+import insurances from './insurances/insurances'
+import therapyModalities from './therapy-modalities/therapyModalities'
+import user from './user/user'
 
 export const schemaTypes = [
-    pshychologists,
-    ageSpecialties,
-    conditionSpecialties,
-    insurances,
-    therapyModalities,
+  pshychologists,
+  ageSpecialties,
+  conditionSpecialties,
+  insurances,
+  therapyModalities,
+  user,
 ]
