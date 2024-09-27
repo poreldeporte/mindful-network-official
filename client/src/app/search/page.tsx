b80ea341b840b0df2977fe09a7541adfb3a7ec98
marfogped <martin@poreldeporte.com>
2024-09-27 08:03:11 -0300

    fix(client): Topbar & navbar moved to specific pages

--- client/src/app/search/page.tsx
+++ client/src/app/search/page.tsx
@@ -1,12 +1,16 @@
 import { Suspense } from "react";
 import { SearchWrapper } from "@/routes/search";
+import { Footer, Topbar, MobileTopBar } from "@/components/shared";
 
 export default function Search() {
   return (
     <section className="min-h-screen lg:h-screen max-h-max mt-10 relative">
+      <Topbar />
+      <MobileTopBar />
       <Suspense fallback={<div>loading...</div>}>
         <SearchWrapper />
       </Suspense>
+      <Footer />
     </section>
   );
 }
