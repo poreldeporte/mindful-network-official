a6a64da17266653dc9e98980f6bfd9534d70c374
marfogped <martin@poreldeporte.com>
2024-09-27 08:14:32 -0300

    add(client): JWT secret

--- client/src/config/sanity.config.ts
+++ client/src/config/sanity.config.ts
@@ -1,5 +1,5 @@
 export const sanityConfig = {
-  projectId: process.env.SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   token: process.env.SANITY_TOKEN,
-  dataset: process.env.SANITY_DATASET,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
 };
