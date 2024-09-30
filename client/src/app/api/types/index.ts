0db5cd09624c812943303e317c2b23757771dece
marfogped <martin@poreldeporte.com>
2024-09-30 18:00:38 -0300

    chore(client): psychologists calls moved to resources folder, & types moved to root

--- client/src/app/api/types/index.ts
+++ client/src/app/api/types/index.ts
@@ -1,5 +1,5 @@
 export * from "./age-specilaty.type";
 export * from "./condition-specialty.type";
 export * from "./insurances.type";
-export * from "./psychologists.type";
+export * from "./resources";
 export * from "./therapy-modalities.type";
