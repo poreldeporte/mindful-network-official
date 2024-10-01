0a50fb5de11095538ca4adab24bb6445d139e4ff
marfogped <martin@poreldeporte.com>
2024-10-01 20:05:25 -0300

    add(client): resources model & function to generate resource keys

---
+++ client/src/utilities/generate-resource.keys.utility.ts
@@ -0,0 +1,13 @@
+import { ResourcesModel, ResourcesKey } from "@/models";
+
+export const generateResourceKeys = (resources: ResourcesModel) => {
+  return Object.keys(resources).map((key) => {
+    const label = key
+      .replace(/([A-Z])/g, " $1")
+      .replace(/^./, (str) => str.toUpperCase());
+
+    const urlKey = key.replace(/([A-Z])/g, "-$1").toLowerCase();
+    const resource: ResourcesKey = { key: urlKey, label };
+    return resource;
+  });
+};
