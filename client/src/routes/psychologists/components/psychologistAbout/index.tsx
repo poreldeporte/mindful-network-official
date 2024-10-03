dce0824de24d999e133d39bf49fe105be4ef01dc
marfogped <martin@poreldeporte.com>
2024-10-03 18:03:37 -0300

    feat(client): animations for professional detail page & centralized aimations

--- client/src/routes/psychologists/components/psychologistAbout/index.tsx
+++ client/src/routes/psychologists/components/psychologistAbout/index.tsx
@@ -1,61 +1,60 @@
 "use client";
 
-import { Typography } from "@/components/ui";
 import { PsychologistModel } from "@/models";
-import { Section, SectionContent, SectionHeader } from "../section";
+import { Section } from "../section";
 import { PersonStanding, Activity, Brain, ArmchairIcon } from "lucide-react";
 
 export function PsychologistAbout({
-  ageSpecialty,
-  insurances,
-  conditionSpecialty,
-  therapyOptions,
+	ageSpecialty,
+	insurances,
+	conditionSpecialty,
+	therapyOptions,
 }: PsychologistModel) {
-  return (
-    <>
-      <Section
-        id="age-specialty"
-        icon={<PersonStanding className="h-12 w-12" />}
-        title="Age Specialty"
-        items={
-          ageSpecialty?.map((item) => ({ id: item.id, label: item.age })) || []
-        }
-        emptyMessage="No Age Specialty available"
-      />
+	return (
+		<>
+			<Section
+				id="age-specialty"
+				icon={<PersonStanding className="h-12 w-12" />}
+				title="Age Specialty"
+				items={
+					ageSpecialty?.map((item) => ({ id: item.id, label: item.age })) || []
+				}
+				emptyMessage="No Age Specialty available"
+			/>
 
-      <Section
-        id="insurances"
-        icon={<Activity className="h-12 w-12" />}
-        title="Insurances"
-        items={
-          insurances?.map((item) => ({ id: item.id, label: item.name })) || []
-        }
-        emptyMessage="No Insurances available"
-      />
+			<Section
+				id="insurances"
+				icon={<Activity className="h-12 w-12" />}
+				title="Insurances"
+				items={
+					insurances?.map((item) => ({ id: item.id, label: item.name })) || []
+				}
+				emptyMessage="No Insurances available"
+			/>
 
-      <Section
-        id="condition-specialty"
-        icon={<Brain className="h-12 w-12" />}
-        title="Condition Specialty"
-        items={
-          conditionSpecialty?.map((item) => ({
-            id: item.id,
-            label: item.name,
-          })) || []
-        }
-        emptyMessage="No Conditions available"
-      />
+			<Section
+				id="condition-specialty"
+				icon={<Brain className="h-12 w-12" />}
+				title="Condition Specialty"
+				items={
+					conditionSpecialty?.map((item) => ({
+						id: item.id,
+						label: item.name,
+					})) || []
+				}
+				emptyMessage="No Conditions available"
+			/>
 
-      <Section
-        id="therapy-options"
-        icon={<ArmchairIcon className="h-12 w-12" />}
-        title="Therapy Options"
-        items={
-          therapyOptions?.map((item) => ({ id: item.id, label: item.type })) ||
-          []
-        }
-        emptyMessage="No Therapy Options available"
-      />
-    </>
-  );
+			<Section
+				id="therapy-options"
+				icon={<ArmchairIcon className="h-12 w-12" />}
+				title="Therapy Options"
+				items={
+					therapyOptions?.map((item) => ({ id: item.id, label: item.type })) ||
+					[]
+				}
+				emptyMessage="No Therapy Options available"
+			/>
+		</>
+	);
 }
--- client/src/routes/psychologists/components/section/index.tsx
+++ client/src/routes/psychologists/components/section/index.tsx
@@ -1,83 +1,118 @@
-"use client"
+"use client";
 
 import { Typography } from "@/components/ui";
 import { useState } from "react";
-import { ChevronDown, ChevronUp } from "lucide-react";
+import { ChevronDown } from "lucide-react";
+import { AnimatePresence, motion } from "framer-motion";
+import { opacityVariants } from "@/lib/anim";
 
 interface SectionProps {
-  id: string;
-  icon: React.ReactNode;
-  title: string;
-  items: Array<{ id: string; label: string }>;
-  emptyMessage: string;
+	id: string;
+	icon: React.ReactNode;
+	title: string;
+	items: Array<{ id: string; label: string }>;
+	emptyMessage: string;
 }
 
-export function Section({ id, icon, title, items, emptyMessage }: SectionProps) {
+export function Section({
+	id,
+	icon,
+	title,
+	items,
+	emptyMessage,
+}: SectionProps) {
+	const [isOpen, setIsOpen] = useState(true);
 
-  const [isOpen, setIsOpen] = useState(true)
+	const toggleContent = () => {
+		setIsOpen(!isOpen);
+	};
 
-  const toggleContent = () => {
-    setIsOpen(!isOpen);
-  }
-
-  return (
-    <section id={id} className="py-10 px-10 lg:rounded-2xl my-10 bg-white shadow-sm shadow-gray-100">
-      <SectionHeader icon={icon} title={title} isOpen={isOpen} toggleContent={toggleContent} />
-      {isOpen && (
-        <SectionContent>
-          <ul className="grid grid-cols-1 gap-4 w-full">
-            {items && items.length ? (
-              items.map((item) => (
-                <li key={item.id} className="flex justify-between items-center border-b py-2">
-                  <Typography as="p" variant="medium" color="darkGray">
-                    {item.label}
-                  </Typography>
-                </li>
-              ))
-            ) : (
-              <Typography as="p" variant="medium" color="darkGray">
-                {emptyMessage}
-              </Typography>
-            )}
-          </ul>
-        </SectionContent>
-      )}
-    </section>
-  );
+	return (
+		<section
+			id={id}
+			className="py-10 px-10 lg:rounded-2xl my-10 bg-white shadow-sm shadow-gray-100 transition-all h-max"
+		>
+			<SectionHeader
+				icon={icon}
+				title={title}
+				isOpen={isOpen}
+				toggleContent={toggleContent}
+			/>
+			<AnimatePresence mode="wait">
+				{isOpen && (
+					<SectionContent>
+						<ul className="grid grid-cols-1 gap-4 w-full">
+							{items && items.length ? (
+								items.map((item) => (
+									<li
+										key={item.id}
+										className="flex justify-between items-center py-2"
+									>
+										<Typography as="p" variant="medium" color="darkGray">
+											{item.label}
+										</Typography>
+									</li>
+								))
+							) : (
+								<Typography as="p" variant="medium" color="darkGray">
+									{emptyMessage}
+								</Typography>
+							)}
+						</ul>
+					</SectionContent>
+				)}
+			</AnimatePresence>
+		</section>
+	);
 }
 
 interface SectionHeaderProps {
-  icon: React.ReactNode;
-  title: string;
-  isOpen: boolean;
-  toggleContent: () => void;
+	icon: React.ReactNode;
+	title: string;
+	isOpen: boolean;
+	toggleContent: () => void;
 }
 
-export const SectionHeader = ({ icon, title, isOpen, toggleContent }: SectionHeaderProps) => {
-  return (
-    <div
-      className="flex items-center justify-between border-b pb-5 border-gray-200 cursor-pointer"
-      onClick={toggleContent}
-    >
-      <div className="flex items-center space-x-3">
-        {icon}
-        <Typography variant="medium" className="font-bold" as="h3" color="black">
-          {title}
-        </Typography>
-      </div>
+export const SectionHeader = ({
+	icon,
+	title,
+	isOpen,
+	toggleContent,
+}: SectionHeaderProps) => {
+	return (
+		<div
+			className="flex items-center justify-between border-b pb-5 border-gray-200 cursor-pointer"
+			onClick={toggleContent}
+		>
+			<div className="flex items-center space-x-3">
+				{icon}
+				<Typography
+					variant="medium"
+					className="font-bold"
+					as="h3"
+					color="black"
+				>
+					{title}
+				</Typography>
+			</div>
 
-      <div>
-        {isOpen ? (
-          <ChevronUp className="h-6 w-6 text-gray-300" />
-        ) : (
-          <ChevronDown className="h-6 w-6 text-gray-300" />
-        )}
-      </div>
-    </div>
-  );
+			<ChevronDown
+				className={`${isOpen ? "rotate-180" : "rotate-0"} h-6 w-6 text-gray-300 transition-transform`}
+			/>
+		</div>
+	);
 };
 
 export const SectionContent = ({ children }: { children: React.ReactNode }) => {
-  return <div className="lg:flex gap-20 mt-10">{children}</div>;
+	return (
+		<motion.div
+			className="lg:flex gap-20 mt-10"
+			initial="closed"
+			animate="open"
+			exit="closed"
+			variants={opacityVariants}
+		>
+			{children}
+		</motion.div>
+	);
 };
-
