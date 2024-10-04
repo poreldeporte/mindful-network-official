0769d688203146de4d6f6efd1bc44deefce67e59
marfogped <martin@poreldeporte.com>
2024-10-04 10:52:37 -0300

    fix(client): About images responisve & Mobile Navbar options

--- client/src/routes/homepage/components/about/index.tsx
+++ client/src/routes/homepage/components/about/index.tsx
@@ -4,81 +4,81 @@ import { Person1, Person2, Person3 } from "@/lib/images";
 import { benefits } from "@/lib/constants";
 
 interface BenefitCardProps {
-  key: number;
-  title: string;
-  description: string;
+	key: number;
+	title: string;
+	description: string;
 }
 
 const BenefitCard = ({ title, description }: BenefitCardProps) => {
-  return (
-    <article>
-      <Typography
-        className="mb-2 font-semibold"
-        color="black"
-        as="h4"
-        variant="medium"
-      >
-        {title}
-      </Typography>
+	return (
+		<article>
+			<Typography
+				className="mb-2 font-semibold"
+				color="black"
+				as="h4"
+				variant="medium"
+			>
+				{title}
+			</Typography>
 
-      <Typography className="mb-2" color="darkGray" as="p" variant="small">
-        {description}
-      </Typography>
-    </article>
-  );
+			<Typography className="mb-2" color="darkGray" as="p" variant="small">
+				{description}
+			</Typography>
+		</article>
+	);
 };
 
 export function About() {
-  return (
-    <section className="page-width section-y-padding bg-gray-50">
-      <Typography className="mb-2" color="black" as="h2" variant="title">
-        <span className="text-green-300">We&apos;re here to help</span>{" "}
-        <span className="block" />
-        build your support net
-      </Typography>
-      <div className="grid grid-cols-1 lg:grid-cols-2 mt-10 lg:mt-20 gap-5">
-        <div className="flex flex-col gap-5">
-          <Typography
-            className="mb-2 font-medium w-3/4"
-            color="black"
-            as="h3"
-            variant="large"
-          >
-            Access the most reliable and reputable mental health resources
-            available
-          </Typography>
+	return (
+		<section className="page-width section-y-padding bg-gray-50">
+			<Typography className="mb-2" color="black" as="h2" variant="title">
+				<span className="text-green-300">We&apos;re here to help</span>{" "}
+				<span className="block" />
+				build your support net
+			</Typography>
+			<div className="grid grid-cols-1 xl:grid-cols-2 mt-10 lg:mt-20 gap-5">
+				<div className="flex flex-col gap-5">
+					<Typography
+						className="mb-2 font-medium w-3/4"
+						color="black"
+						as="h3"
+						variant="large"
+					>
+						Access the most reliable and reputable mental health resources
+						available
+					</Typography>
 
-          <div className="my-5">
-            {benefits.map((benefit) => {
-              return (
-                <BenefitCard
-                  key={benefit.key}
-                  title={benefit.title}
-                  description={benefit.description}
-                />
-              );
-            })}
-          </div>
-        </div>
+					<div className="my-5">
+						{benefits.map((benefit) => {
+							return (
+								<BenefitCard
+									key={benefit.key}
+									title={benefit.title}
+									description={benefit.description}
+								/>
+							);
+						})}
+					</div>
+				</div>
 
-        <div className="grid grid-cols-3 lg:flex gap-5 w-full h-full relative">
-          <Image
-            className="w-auto h-auto lg:absolute lg:top-0 lg:left-0 lg:w-[370px] lg:h-auto"
-            src={Person1}
-            alt="Person 1"
-          />
-          <Image
-            className="w-auto h-auto lg:absolute lg:-bottom-20 lg:w-[200px] lg:left-1/4 lg:h-auto"
-            src={Person2}
-            alt="Person 2"
-          />
-          <Image
-            className="w-auto h-auto lg:absolute lg:bottom-20 lg:w-[270px] lg:right-20 lg:h-auto"
-            src={Person3}
-            alt="Person 3"
-          />
-        </div>
-      </div>
-    </section>
-  );
+				<div className="grid grid-cols-3 xl:flex gap-5 w-full h-full relative">
+					<Image
+						className="w-auto h-auto xl:absolute xl:top-0 xl:left-0 xl:w-[370px] xl:h-auto"
+						src={Person1}
+						alt="Person 1"
+					/>
+					<Image
+						className="w-auto h-full xl:absolute xl:-bottom-20 xl:w-[200px] xl:left-1/4 xl:h-auto"
+						src={Person2}
+						alt="Person 2"
+					/>
+					<Image
+						className="w-auto h-auto xl:absolute xl:bottom-20 xl:w-[270px] xl:right-20 xl:h-auto"
+						src={Person3}
+						alt="Person 3"
+					/>
+				</div>
+			</div>
+		</section>
+	);
 }
