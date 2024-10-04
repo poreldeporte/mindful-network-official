9c51938dfa9b950a12ca71d3ad27736c9fe014b5
marfogped <martin@poreldeporte.com>
2024-10-04 10:35:10 -0300

    fix(client): not used imports and props, and  props validations to avoid linter issues

--- client/src/routes/psychologists/components/profileCard/index.tsx
+++ client/src/routes/psychologists/components/profileCard/index.tsx
@@ -7,69 +7,68 @@ import { ShieldPlus, CircleCheck } from "lucide-react";
 import { formatType } from "@/utilities";
 
 export function ProfileCard({
-  image,
-  conditionSpecialty,
-  name,
-  description,
-  _type,
+	image,
+	name,
+	description,
+	_type,
 }: PsychologistModel) {
-  return (
-    <header className="mb-10 mt-32 justify-center bg-white rounded-2xl p-10 lg:mt-0">
-      <div className="flex items-center justify-start">
-        <div className="w-24 h-24 lg:h-48 lg:w-48 mr-5 mb-5">
-          <Image
-            className="rounded-full aspect-square object-cover"
-            src={image ? image : UserImage}
-            alt={name}
-            width={300}
-            height={300}
-          />
-        </div>
-        <div>
-          <Typography
-            className="font-bold mb-5"
-            as="h2"
-            variant="medium"
-            color="black"
-          >
-            {name}
-          </Typography>
-          <div>
-            <Badge className="mr-2 mb-2" color="blue" isSelected={false}>
-              {formatType(_type)}
-            </Badge>
-          </div>
-        </div>
-      </div>
-      <div className="my-5">
-        <Typography className="mb-5" as="h2" variant="small" color="darkGray">
-          {description}
-        </Typography>
-      </div>
-      <div className="flex flex-col space-y-3 lg:items-center lg:justify-start lg:flex-row lg:space-x-6 lg:space-y-0">
-        <div className="flex items-center space-x-2">
-          <ShieldPlus className="w-10 h-10" />
-          <Typography
-            className="font-semibold"
-            as="p"
-            variant="small"
-            color="darkGray"
-          >
-            Accepts Insurance
-          </Typography>
-        </div>
-        <div className="flex items-center space-x-2">
-          <CircleCheck className="w-10 h-10" />
-          <Typography
-            className="font-semibold"
-            as="p"
-            variant="small"
-            color="darkGray"
-          >
-            Vetted & Verified
-          </Typography>
-        </div>
-      </div>
-    </header>
-  );
+	return (
+		<header className="mb-10 mt-32 justify-center bg-white rounded-2xl p-10 lg:mt-0">
+			<div className="flex items-center justify-start">
+				<div className="w-24 h-24 lg:h-48 lg:w-48 mr-5 mb-5">
+					<Image
+						className="rounded-full aspect-square object-cover"
+						src={image ? image : UserImage}
+						alt={name}
+						width={300}
+						height={300}
+					/>
+				</div>
+				<div>
+					<Typography
+						className="font-bold mb-5"
+						as="h2"
+						variant="medium"
+						color="black"
+					>
+						{name}
+					</Typography>
+					<div>
+						<Badge className="mr-2 mb-2" color="blue" isSelected={false}>
+							{formatType(_type)}
+						</Badge>
+					</div>
+				</div>
+			</div>
+			<div className="my-5">
+				<Typography className="mb-5" as="h2" variant="small" color="darkGray">
+					{description}
+				</Typography>
+			</div>
+			<div className="flex flex-col space-y-3 lg:items-center lg:justify-start lg:flex-row lg:space-x-6 lg:space-y-0">
+				<div className="flex items-center space-x-2">
+					<ShieldPlus className="w-10 h-10" />
+					<Typography
+						className="font-semibold"
+						as="p"
+						variant="small"
+						color="darkGray"
+					>
+						Accepts Insurance
+					</Typography>
+				</div>
+				<div className="flex items-center space-x-2">
+					<CircleCheck className="w-10 h-10" />
+					<Typography
+						className="font-semibold"
+						as="p"
+						variant="small"
+						color="darkGray"
+					>
+						Vetted & Verified
+					</Typography>
+				</div>
+			</div>
+		</header>
+	);
 }
--- client/src/routes/search/components/search-wrapper/side-panel/SidePanel.tsx
+++ client/src/routes/search/components/search-wrapper/side-panel/SidePanel.tsx
@@ -1,186 +1,185 @@
 "use client";
 
-import { Badge, Typography, NoResults } from "@/components/ui";
+import { NoResults, Typography } from "@/components/ui";
 import {
-  conditionSpecialty,
-  insurances,
-  PsychologistModel,
-  ResourcesKey,
-  TherapyModality,
+	conditionSpecialty,
+	insurances,
+	PsychologistModel,
+	ResourcesKey,
+	TherapyModality,
 } from "@/models";
 import { useRouter, useSearchParams } from "next/navigation";
-import PsychologistCard from "./PsychologistCard";
-import { useState, useEffect } from "react";
-import { PsychologistCardSkeleton } from "./PsychologistCard.skeleton";
+import { useEffect, useState } from "react";
 import Header from "./Header";
+import PsychologistCard from "./PsychologistCard";
+import { PsychologistCardSkeleton } from "./PsychologistCard.skeleton";
 
 interface Props {
-  proffesionals: PsychologistModel[] | null;
-  filteredProffesionals: PsychologistModel[] | null;
-  conditions: conditionSpecialty[] | null;
-  insurances: insurances[] | null;
-  therapyModalities: TherapyModality[] | null;
-  resources: ResourcesKey[];
-  isLoading: Boolean;
+	proffesionals: PsychologistModel[] | null;
+	filteredProffesionals: PsychologistModel[] | null;
+	conditions: conditionSpecialty[] | null;
+	insurances: insurances[] | null;
+	therapyModalities: TherapyModality[] | null;
+	resources: ResourcesKey[];
+	isLoading: boolean;
 }
 
 const SidePanel = ({
-  proffesionals,
-  filteredProffesionals,
-  conditions,
-  insurances,
-  therapyModalities,
-  resources,
-  isLoading
+	proffesionals,
+	filteredProffesionals,
+	conditions,
+	insurances,
+	therapyModalities,
+	resources,
+	isLoading,
 }: Props) => {
-  const [selectedCondition, setSelectedCondition] = useState<string[]>([]);
+	const [selectedCondition, setSelectedCondition] = useState<string[]>([]);
 
-  const [selectedResources, setSelectedResources] = useState<string[]>([]);
-  const [selectedInsurance, setSelectedInsurance] = useState<string[]>([]);
-  const [selectedTherapy, setSelectedTherapy] = useState<string | null>(null);
+	const [selectedResources, setSelectedResources] = useState<string[]>([]);
+	const [selectedInsurance, setSelectedInsurance] = useState<string[]>([]);
+	const [selectedTherapy, setSelectedTherapy] = useState<string | null>(null);
 
-  const searchParams = useSearchParams();
-  const router = useRouter();
+	const searchParams = useSearchParams();
+	const router = useRouter();
 
-  useEffect(() => {
-    const conditionParam = searchParams.get("condition");
-    const insuranceParam = searchParams.get("insurance");
-    const therapyParam = searchParams.get("therapy");
-    const resourceParam = searchParams.get("resource");
+	useEffect(() => {
+		const conditionParam = searchParams.get("condition");
+		const insuranceParam = searchParams.get("insurance");
+		const therapyParam = searchParams.get("therapy");
+		const resourceParam = searchParams.get("resource");
 
-    if (conditionParam) {
-        setSelectedCondition(conditionParam.split(","));
-    }
+		if (conditionParam) {
+			setSelectedCondition(conditionParam.split(","));
+		}
 
-    if (insuranceParam) {
-        setSelectedInsurance(insuranceParam.split(","));
-    }
+		if (insuranceParam) {
+			setSelectedInsurance(insuranceParam.split(","));
+		}
 
-    if (therapyParam) {
-        setSelectedTherapy(therapyParam);
-    }
+		if (therapyParam) {
+			setSelectedTherapy(therapyParam);
+		}
 
-    if (resourceParam) {
-        setSelectedResources(resourceParam.split(","));
-    }
-}, [searchParams]);
+		if (resourceParam) {
+			setSelectedResources(resourceParam.split(","));
+		}
+	}, [searchParams]);
 
-const handleBadgeClick = (filterType: string, value: string) => {
-  const currentParams = new URLSearchParams(searchParams.toString());
-  
-  if (filterType === "resource") {
-  let updatedResources = [...selectedResources];
-  
-  if (updatedResources.includes(value)) {
-  updatedResources = updatedResources.filter(
-  (resource) => resource !== value
-  );
-  } else {
-  updatedResources.push(value);
-  }
-  
-  setSelectedResources(updatedResources);
-  
-  if (updatedResources.length > 0) {
-  currentParams.set("resource", updatedResources.join(","));
-  } else {
-  currentParams.delete("resource");
-  }
-  } else if (filterType === "insurance") {
-  let selectedInsurances = selectedInsurance ? [...selectedInsurance] : [];
-  
-  if (selectedInsurances.includes(value)) {
-  selectedInsurances = selectedInsurances.filter(
-  (insurance) => insurance !== value
-  );
-  } else {
-  selectedInsurances.push(value);
-  }
-  
-  if (selectedInsurances.length > 0) {
-  currentParams.set("insurance", selectedInsurances.join(","));
-  } else {
-  currentParams.delete("insurance");
-  }
-  
-  setSelectedInsurance(selectedInsurances);
-  } else if (filterType === "condition") {
-  let updatedConditions = [...selectedCondition];
-  
-  if (updatedConditions.includes(value)) {
-  updatedConditions = updatedConditions.filter(
-  (condition) => condition !== value
-  );
-  } else {
-  updatedConditions.push(value);
-  }
-  
-  setSelectedCondition(updatedConditions);
-  
-  if (updatedConditions.length > 0) {
-  currentParams.set("condition", updatedConditions.join(","));
-  } else {
-  currentParams.delete("condition");
-  }
-  } else if (filterType === "therapy") {
-  setSelectedTherapy(selectedTherapy === value ? null : value);
-  if (currentParams.get(filterType) === value) {
-  currentParams.delete(filterType);
-  } else {
-  currentParams.set(filterType, value);
-  }
-  }
-  
-  router.push(`?${currentParams.toString()}`, undefined);
-  };
+	const handleBadgeClick = (filterType: string, value: string) => {
+		const currentParams = new URLSearchParams(searchParams.toString());
 
-  return (
-    <aside className="overflow-hidden z-10 lg:py-5 pb-5 pt-14 lg:absolute lg:left-2.5 lg:top-1/2 lg:-translate-y-1/2 h-max lg:h-[calc(100%-20px)] w-full lg:w-1/2 bg-white rounded-3xl grid grid-rows-[auto_1fr_auto]">
-      <Header
-                conditions={conditions}
-                handleBadgeClick={handleBadgeClick}
-                insurances={insurances}
-                resources={resources}
-                therapyModalities={therapyModalities}
-                selectedCondition={selectedCondition}
-                selectedInsurance={selectedInsurance}
-                selectedResources={selectedResources}
-                selectedTherapy={selectedTherapy}
-      />
-      <div className="overflow-y-auto overflow-x-hidden max-w-full">
-        
-          {isLoading ? (
-            <>
-              {Array(5).fill(0).map((_, index) => (
-                <PsychologistCardSkeleton key={index} />
-              ))}
-            </>
-          ) : filteredProffesionals && filteredProffesionals.length ? (
-            <ul className="px-5 divide-y divide-gray-200">
-            {
-            filteredProffesionals.map((psychologist) => (
-              <PsychologistCard
-                psychologist={psychologist}
-                key={psychologist.id}
-              />
-            ))}
-            </ul>
-          ) : (
-            <NoResults title="No results found"/>
-          )}
-        
-      </div>
+		if (filterType === "resource") {
+			let updatedResources = [...selectedResources];
 
-      <footer className="px-5 pt-2.5 flex items-center">
-        {filteredProffesionals && proffesionals && (
-          <Typography as="span" color="black" variant="xsmall">
-            Showing {filteredProffesionals.length} of {proffesionals.length}{" "}
-            professionals
-          </Typography>
-        )}
-      </footer>
-    </aside>
-  );
+			if (updatedResources.includes(value)) {
+				updatedResources = updatedResources.filter(
+					(resource) => resource !== value
+				);
+			} else {
+				updatedResources.push(value);
+			}
+
+			setSelectedResources(updatedResources);
+
+			if (updatedResources.length > 0) {
+				currentParams.set("resource", updatedResources.join(","));
+			} else {
+				currentParams.delete("resource");
+			}
+		} else if (filterType === "insurance") {
+			let selectedInsurances = selectedInsurance ? [...selectedInsurance] : [];
+
+			if (selectedInsurances.includes(value)) {
+				selectedInsurances = selectedInsurances.filter(
+					(insurance) => insurance !== value
+				);
+			} else {
+				selectedInsurances.push(value);
+			}
+
+			if (selectedInsurances.length > 0) {
+				currentParams.set("insurance", selectedInsurances.join(","));
+			} else {
+				currentParams.delete("insurance");
+			}
+
+			setSelectedInsurance(selectedInsurances);
+		} else if (filterType === "condition") {
+			let updatedConditions = [...selectedCondition];
+
+			if (updatedConditions.includes(value)) {
+				updatedConditions = updatedConditions.filter(
+					(condition) => condition !== value
+				);
+			} else {
+				updatedConditions.push(value);
+			}
+
+			setSelectedCondition(updatedConditions);
+
+			if (updatedConditions.length > 0) {
+				currentParams.set("condition", updatedConditions.join(","));
+			} else {
+				currentParams.delete("condition");
+			}
+		} else if (filterType === "therapy") {
+			setSelectedTherapy(selectedTherapy === value ? null : value);
+			if (currentParams.get(filterType) === value) {
+				currentParams.delete(filterType);
+			} else {
+				currentParams.set(filterType, value);
+			}
+		}
+
+		router.push(`?${currentParams.toString()}`, undefined);
+	};
+
+	return (
+		<aside className="overflow-hidden z-10 lg:py-5 pb-5 pt-14 lg:absolute lg:left-2.5 lg:top-1/2 lg:-translate-y-1/2 h-max lg:h-[calc(100%-20px)] w-full lg:w-1/2 bg-white rounded-3xl grid grid-rows-[auto_1fr_auto]">
+			<Header
+				conditions={conditions}
+				handleBadgeClick={handleBadgeClick}
+				insurances={insurances}
+				resources={resources}
+				therapyModalities={therapyModalities}
+				selectedCondition={selectedCondition}
+				selectedInsurance={selectedInsurance}
+				selectedResources={selectedResources}
+				selectedTherapy={selectedTherapy}
+			/>
+			<div className="overflow-y-auto overflow-x-hidden max-w-full">
+				{isLoading ? (
+					<>
+						{Array(5)
+							.fill(0)
+							.map((_, index) => (
+								<PsychologistCardSkeleton key={index} />
+							))}
+					</>
+				) : filteredProffesionals && filteredProffesionals.length ? (
+					<ul className="px-5 divide-y divide-gray-200">
+						{filteredProffesionals.map((psychologist) => (
+							<PsychologistCard
+								psychologist={psychologist}
+								key={psychologist.id}
+							/>
+						))}
+					</ul>
+				) : (
+					<NoResults title="No results found" />
+				)}
+			</div>
+
+			<footer className="px-5 pt-2.5 flex items-center">
+				{filteredProffesionals && proffesionals && (
+					<Typography as="span" color="black" variant="xsmall">
+						Showing {filteredProffesionals.length} of {proffesionals.length}{" "}
+						professionals
+					</Typography>
+				)}
+			</footer>
+		</aside>
+	);
 };
 
 export default SidePanel;
