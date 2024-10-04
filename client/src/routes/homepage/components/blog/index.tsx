dea9a1c7597336f58b99eb008e3e8a931970a574
marfogped <martin@poreldeporte.com>
2024-10-04 18:34:50 -0300

    feat(client): Blog section now works with real Sanity Data

--- client/src/routes/homepage/components/blog/index.tsx
+++ client/src/routes/homepage/components/blog/index.tsx
@@ -1,58 +1,57 @@
-import { BlogModel } from "@/models/blog.model";
-import { blogPosts } from "@/lib/constants";
 import { Typography } from "@/components/ui";
+import { BlogModel } from "@/models/blog.model";
 import Image from "next/image";
-import React from "react";
+import Link from "next/link";
 
-const BlogCard = ({ title, description, image, slug }: BlogModel) => {
-  return (
-    <article className="w-full">
-      <a href={slug}>
-        <Image
-          className="w-full aspect-video object-cover mb-4 rounded-md"
-          src={image}
-          width={300}
-          height={300}
-          alt={`${title} image`}
-        />
-        <Typography color="darkGray" as="h3" variant="small">
-          {title}
-        </Typography>
-        <Typography
-          className="mb-2 font-semibold"
-          color="black"
-          as="h2"
-          variant="medium"
-        >
-          {description}
-        </Typography>
-      </a>
-    </article>
-  );
-};
-
-interface Props {
-  posts?: BlogModel[] | null;
+interface BlogProps {
+	blogPosts: BlogModel[];
 }
 
-export const BlogContainer: React.FC<Props> = () => {
-  return (
-    <section className="page-width section-y-padding">
-      <Typography
-        className="mb-20 font-medium leading-tight"
-        color="black"
-        as="h2"
-        variant="title"
-      >
-        Expert articles and resources <span className="block"></span>to
-        <span className="text-green-300"> support your journey</span>
-      </Typography>
+const BlogCard = ({ title, category, featuredImage, slug }: BlogModel) => (
+	<article className="w-full">
+		<Link href={`/blog/${slug}`}>
+			<Image
+				className="w-full aspect-video object-cover mb-4 rounded-md"
+				src={featuredImage}
+				width={300}
+				height={300}
+				alt={`${title} image`}
+			/>
+			<Typography color="darkGray" as="h3" variant="small">
+				{category}
+			</Typography>
+			<Typography
+				className="mb-2 font-semibold"
+				color="black"
+				as="h2"
+				variant="medium"
+			>
+				{title}
+			</Typography>
+		</Link>
+	</article>
+);
 
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5 lg:gap-5">
-        {blogPosts.map((blogPost) => (
-          <BlogCard key={blogPost.id} {...blogPost} />
-        ))}
-      </div>
-    </section>
-  );
+export const BlogContainer = ({ blogPosts }: BlogProps) => {
+	return (
+		<section className="page-width section-y-padding">
+			<Typography
+				className="mb-20 font-medium leading-tight"
+				color="black"
+				as="h2"
+				variant="title"
+			>
+				Expert articles and resources <span className="block"></span>to
+				<span className="text-green-300"> support your journey</span>
+			</Typography>
+
+			<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5 lg:gap-5">
+				{blogPosts && blogPosts.length
+					? blogPosts.map((blogPost) => (
+							<BlogCard key={blogPost.slug} {...blogPost} />
+						))
+					: ""}
+			</div>
+		</section>
+	);
 };
