4d576f4e868b7c6b481c801358e6d1cc9caf4dea
LucianoPED <luciano@poreldeporte.com>
2024-10-10 18:41:08 -0300

    Feat(Client): Adding Hero for Blogs page

--- client/src/lib/images.ts
+++ client/src/lib/images.ts
@@ -1,6 +1,7 @@
 import Person1 from "@/assets/images/person-1.webp";
 import Person2 from "@/assets/images/person-2.webp";
 import Person3 from "@/assets/images/person-3.webp";
+import BlogsHeroImage from "@/assets/images/blogsHero.webp";
 import UserImage from "@/assets/images/user-icon.jpg";
 
 import MindfulImagotype from "@/assets/images/mindful-logos/mindful-imagotype.png";
@@ -18,4 +19,5 @@ export {
 	MindfulIsotype,
 	MindfulLogo,
 	AboutImage,
+	BlogsHeroImage,
 };
--- client/src/routes/blogs-page/components/hero/index.tsx
+++ client/src/routes/blogs-page/components/hero/index.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { BlogsHeroImage } from "@/lib/images";
+import Image from "next/image";
+import { Typography } from "@/components/ui";
 
 export const BlogsHero = () => {
-	return <section>Hero</section>;
+	return (
+		<section className="h-[60vh] flex items-center justify-center p-2 relative">
+			<Image
+				src={BlogsHeroImage}
+				alt="blogs-hero-image"
+				className="w-[calc(100%-8px)] h-full object-cover object-center rounded-xl absolute left-1/2 -translate-x-1/2"
+			/>
+			<div className="flex flex-col z-10">
+				<Typography className="" color="white" as="h2" variant="large">
+					Explorer the wonders of mental health
+				</Typography>
+			</div>
+		</section>
+	);
 };
