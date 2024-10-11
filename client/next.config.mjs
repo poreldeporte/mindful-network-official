ff7db9ad7df97418e869b59f3f2f220ebd401fc3
marfogped <martin@poreldeporte.com>
2024-10-11 15:16:49 -0300

    core(client): next images optimization removed

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["plus.unsplash.com", "images.unsplash.com", "cdn.sanity.io"],
-  },
+	images: {
+		domains: ["plus.unsplash.com", "images.unsplash.com", "cdn.sanity.io"],
+		unoptimized: true,
+	},
 };
 
 export default nextConfig;
