7f2f5975e966531553f78ebf23b1da80208099cc
marfogped <martin@poreldeporte.com>
2024-10-31 16:48:47 -0300

    fix: Useful Links page as use client component

--- client/src/app/useful-links/page.tsx
+++ client/src/app/useful-links/page.tsx
@@ -1,9 +1,27 @@
+"use client";
+
 import { Topbar, Footer, MobileTopBar } from "@/components/shared";
+import { UseFulLinkSection } from "@/models";
 import { UseFulLinksContent } from "@/routes/useful-links";
-import { getUsefulLinks } from "@/services/";
+import { getUsefulLinks } from "@/services";
+import { useState, useEffect } from "react";
 
 export default async function UsefulLinks() {
-	const usefulLinksSections = await getUsefulLinks();
+	const [usefulLinksSections, setUsefulLinksSections] = useState<
+		UseFulLinkSection[] | []
+	>([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const data = await getUsefulLinks();
+				if (data) setUsefulLinksSections(data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchData();
+	}, []);
 
 	return (
 		<>
