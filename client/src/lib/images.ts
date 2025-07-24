3cb99d4c51d4b487f66bdff8adafd71007b0a80a
marfogped <martin@poreldeporte.com>
2025-07-24 12:16:02 -0300

    add(client): New images

--- client/src/lib/images.ts
+++ client/src/lib/images.ts
@@ -1,11 +1,17 @@
-import Person1 from "@/assets/images/person-1.webp";
-import Person2 from "@/assets/images/person-2.webp";
-import Person3 from "@/assets/images/person-3.webp";
+import HeroImage from "@/assets/images/hero.webp";
+import CoachingAndSupportImage from "@/assets/images/coaching-and-support.webp";
+import PersonalizedSearchOptionsImage from "@/assets/images/personalized-search-options.webp";
+import TrustedAndVerifiedResourcesImage from "@/assets/images/trusted-and-verified-resources.webp";
+import UpToDateInformationImage from "@/assets/images/up-to-date-information.webp";
+
 import UserImage from "@/assets/images/user-icon.jpg";
 
 import MindfulImagotype from "@/assets/images/mindful-logos/mindful-imagotype.png";
 import MindfulIsotype from "@/assets/images/mindful-logos/mindful-isotype.png";
 import MindfulLogo from "@/assets/images/mindful-logos/mindful-logo.png";
+
+import ExploreCuratedResourcesImage from "@/assets/images/explore-curated-resources.webp";
+import JoinOurNetworkImage from "@/assets/images/join-our-network.webp";
 
 import AboutImage from "@/assets/images/mindful-about-image.webp";
 
@@ -18,9 +24,6 @@ import MentalHealthCrisisImage from "@/assets/images/mental-health-crisis.webp";
 import EventsHeroImage from "@/assets/images/events/events-hero-bkg.webp";
 
 export {
-	Person1,
-	Person2,
-	Person3,
 	UserImage,
 	MindfulImagotype,
 	MindfulIsotype,
@@ -31,4 +34,11 @@ export {
 	SearchCtaBlogImage,
 	MentalHealthCrisisImage,
 	EventsHeroImage,
+	CoachingAndSupportImage,
+	PersonalizedSearchOptionsImage,
+	TrustedAndVerifiedResourcesImage,
+	UpToDateInformationImage,
+	HeroImage,
+	ExploreCuratedResourcesImage,
+	JoinOurNetworkImage,
 };
